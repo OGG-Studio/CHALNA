44d5eed8d0ef6d0f2ca979447e49517f90a1f19d
HaeWoong Jang <leap1568@gmail.com>
2018-05-09 12:53:52 +0900

    [D] cameraTest

--- openCVLibrary341/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary341/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: C:\\Users\\philip\\AndroidStudioProjects\\CHALNA\\openCVLibrary341\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: C:\\Users\\LEaps\\AndroidStudioProjects\\CHALNA\\openCVLibrary341\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
