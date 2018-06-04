96618cbac2c49398063d2dae2d5d2e5f74d92bae
leng <wonwoowon21@gmail.com>
2018-06-05 02:53:40 +0900

    made main setting screen
    create main setting screen with introductio of developers

---
+++ yongrammer/chalna_setting/app/src/main/java/com/example/leng/chalna_setting/introductionView.java
@@ -0,0 +1,16 @@
+package com.example.leng.chalna_setting;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+public class introductionView extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.introductionview);
+    }
+
+
+}
